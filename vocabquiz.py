05666e8787c06f1e992d9abf161b7869c305165c
brentzucker <brentzucker@gmail.com>
2015-04-25 12:30:01 -0400

    Generates random dictionary

--- vocabquiz.py
+++ vocabquiz.py
@@ -4,7 +4,7 @@
 
 #1. Check if there are any vocabulary files in the directory, which by convention are files that end in .txt. 
 #Print the list of files found. If there aren't files ending in .txt, display an error message and quit.
-import os, inspect
+import os, inspect, random
 
 def printFiles():
 	#directory containing this python file
@@ -47,12 +47,38 @@ def storeVocabWords(file):
 def getNumOfEnglishWords(vocab_dictionary):
 	return len(vocab_dictionary)
 
+#5. Quiz the user by using a randomly generated list of English words from the dictionary.
+def getShuffledDictionary(vocab_dictionary):
+	shuffled_dictionary = {}
+	while len(vocab_dictionary) > 0 :
+		random.seed()
+		#get random number
+		index = random.randint(0, len(vocab_dictionary)-1)
+
+		#key associated with that index
+		english_word = getKeyByIndex(vocab_dictionary, index)
+
+		#add element at that random index to the shuffled dictionary
+		shuffled_dictionary[english_word] = vocab_dictionary[english_word]
+
+		#remove element from dictionary
+		del vocab_dictionary[english_word]
+	return shuffled_dictionary
+
+def getKeyByIndex(vocab_dictionary, index):
+	count = 0
+	for key in vocab_dictionary:
+		if count == index :
+			return key
+		count+=1
+
 #Demo
 print 'Enter which vocab file you would like to use: '
 printFiles()
 file = openFile()
 vocab_dictionary = storeVocabWords(file)
-
+vocab_dictionary = getShuffledDictionary(vocab_dictionary)
+print vocab_dictionary
 for english in vocab_dictionary:
 	print english + ' => ' + vocab_dictionary[english]
 
@@ -61,7 +87,5 @@ num_of_english_words = getNumOfEnglishWords(vocab_dictionary)
 print num_of_english_words
 
 
-
-#5. Quiz the user by using a randomly generated list of English words from the dictionary.
 #6. If the user missed any questions, give the option to output the missed words to a new word list file.
 #7. Print the number of words and scores that the user missed.
