2746d5cecf048a7679ead3e27e3b7fce93b95b43
AlexHeavner <alexander.heavner@bobcats.gcsu.edu>
2015-04-25 13:46:49 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,23 +1,40 @@
-from tkinter import *
+rom tkinter import *
 from functions import*
 
+class GUI:
+	def __init__(self, master):
+		frame = Frame(master)
+		frame.pack()
+
+		choice = IntVar()
+		choice.set(1)
+
+		file_list = avaliableLibraries()
+
+		count = 1
+		if file_list:
+			for file_path in file_list:
+				Radiobutton(root, 
+            				text=file_path,
+            				padx = 20, 
+            				variable=choice, 
+            				value=count).pack(anchor=W)
+				count += 1
+
+			button = Button(root, text='Select', width=25, command=selectAction)
+			button.pack()
+
+
+		else:
+			Label(root, 
+      			text="""No libraries found.""",
+      			justify = LEFT,
+      			padx = 20).pack()
+
+
+	def selectAction():
+		print(choice.get())
 
 root = Tk()
-v = IntVar()
-
-file_list = avaliableLibraries()
-
-if file_list:
-	for file_path in file_list:
-		Radiobutton(root, 
-            		text=file_path,
-            		padx = 20, 
-            		variable=v, 
-            		value=1).pack(anchor=W)
-else:
-	Label(root, 
-      	text="""No libraries found.""",
-      	justify = LEFT,
-      	padx = 20).pack()
-
+gui = GUI(root)
 root.mainloop()
